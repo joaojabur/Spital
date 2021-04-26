c822baa3a3d2db4e97e26b779a6a2cfebf118164
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2021-04-26 18:58:42 -0300

    Updated home to work with PrivateRoute

--- web/src/routes/Router.tsx
+++ web/src/routes/Router.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Landing from "../pages/Landing";
 import LoginAndRegisterMedico from "../pages/LoginAndRegisterMedico";
@@ -18,66 +18,69 @@ import RegisterSpitalAccountCredentials from "../pages/RegisterSpitalAccount/Reg
 import RegisterSpitalAccountPhone from "../pages/RegisterSpitalAccount/RegisterSpitalAccountPhone";
 import RegisterSpitalAccountReview from "../pages/RegisterSpitalAccount/RegisterSpitalAccountReview";
 import HomeClient from "../platform-pages/HomeClient";
+import PrivateRoute from "./PrivateRoute";
 
 //import PrivateRoute from "../components/PrivateRoute";
 
 function Router() {
   return (
     <BrowserRouter>
-      <Route path="/home-cliente" component={HomeClient} />
-      <Route path="/" component={Landing} exact />
-      <Route
-        path="/entrar-registrar-paciente"
-        component={LoginAndRegisterPaciente}
-      />
-      <Route
-        path="/entrar-registrar-medico"
-        component={LoginAndRegisterMedico}
-      />
-      <Route path="/registrar-paciente" component={RegisterPacient} />
-      <Route path="/entrar-paciente" component={LoginPacient} />
-      <Route path="/login-spital-paciente" component={LoginSpitalAccount} />
+      <Switch>
+        <PrivateRoute path="/home-cliente" component={HomeClient} />
+        <Route path="/" component={Landing} exact />
+        <Route
+          path="/entrar-registrar-paciente"
+          component={LoginAndRegisterPaciente}
+        />
+        <Route
+          path="/entrar-registrar-medico"
+          component={LoginAndRegisterMedico}
+        />
+        <Route path="/registrar-paciente" component={RegisterPacient} />
+        <Route path="/entrar-paciente" component={LoginPacient} />
+        <Route path="/login-spital-paciente" component={LoginSpitalAccount} />
 
-      <Route
-        path="/registrar-spital-paciente"
-        component={RegisterSpitalAccount}
-      />
-      <Route
-        path="/registrar-spital-paciente-1"
-        component={RegisterSpitalAccountCredentials}
-      />
-      <Route
-        path="/registrar-spital-paciente-2"
-        component={RegisterSpitalAccountPhone}
-      />
-      <Route
-        path="/registrar-spital-paciente-3"
-        component={RegisterSpitalAccountReview}
-      />
-      <Route
-        path="/registrar-spital-medico"
-        component={RegisterMedicalSpitalNames}
-      />
-      <Route
-        path="/registrar-spital-medico-1"
-        component={RegisterMedicalSpitalCredentials}
-      />
-      <Route
-        path="/registrar-spital-medico-2"
-        component={RegisterMedicalSpitalAcademic}
-      />
-      <Route
-        path="/registrar-spital-medico-3"
-        component={RegisterMedicalSpitalPersonalData}
-      />
-      <Route
-        path="/registrar-spital-medico-4"
-        component={RegisterMedicalSpitalSchedule}
-      />
-      <Route
-        path="/registrar-spital-medico-5"
-        component={RegisterMedicalSpitalAccountReview}
-      />
+        <Route
+          path="/registrar-spital-paciente"
+          component={RegisterSpitalAccount}
+        />
+        <Route
+          path="/registrar-spital-paciente-1"
+          component={RegisterSpitalAccountCredentials}
+        />
+        <Route
+          path="/registrar-spital-paciente-2"
+          component={RegisterSpitalAccountPhone}
+        />
+        <Route
+          path="/registrar-spital-paciente-3"
+          component={RegisterSpitalAccountReview}
+        />
+        <Route
+          path="/registrar-spital-medico"
+          component={RegisterMedicalSpitalNames}
+        />
+        <Route
+          path="/registrar-spital-medico-1"
+          component={RegisterMedicalSpitalCredentials}
+        />
+        <Route
+          path="/registrar-spital-medico-2"
+          component={RegisterMedicalSpitalAcademic}
+        />
+        <Route
+          path="/registrar-spital-medico-3"
+          component={RegisterMedicalSpitalPersonalData}
+        />
+        <Route
+          path="/registrar-spital-medico-4"
+          component={RegisterMedicalSpitalSchedule}
+        />
+        <Route
+          path="/registrar-spital-medico-5"
+          component={RegisterMedicalSpitalAccountReview}
+        />
+      </Switch>
     </BrowserRouter>
   );
 }
