b7c76c98bc58d03bd9100588716bd86016f2200e
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2021-04-27 16:32:08 -0300

    PublicRoute Created

---
+++ web/src/routes/PublicRoute.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Redirect, Route } from 'react-router-dom';
+import { useAuth } from '../context/AuthProvider';
+
+export default function PublicRoute({ component: Component, ...rest}: any) {
+    const { authenticated } = useAuth();
+
+    if (!authenticated){
+        return <Route {...rest} render={props => <Component {...props}/> }/>
+    }
+    
+    return (
+        <Redirect to="/principal"/>
+    );
+}
