6dfc4ef3092f9939501740c28ff668b315179035
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2021-05-05 22:24:52 -0300

    added verifyemail on client

--- server/src/controllers/clientController.js
+++ server/src/controllers/clientController.js
@@ -2,7 +2,7 @@ const knex = require("../database");
 const jwt = require("jsonwebtoken");
 const authConfig = require("../configs/authConfig.json");
 const bcrypt = require("bcrypt");
-const validateTokens = require("../configs/jwt");
+const verifyEmail = require('../services/email/verify');
 
 module.exports = {
   async index(req, res, next) {
@@ -53,6 +53,13 @@ module.exports = {
           userID: parseInt(userID),
         });
 
+        // Tenta enviar o email
+        await verifyEmail({
+          id: userID,
+          email,
+          name: `${firstName} ${lastName}`
+        });
+
         return res.status(201).send();
       }
     } catch (error) {
@@ -73,7 +80,7 @@ module.exports = {
           phoneNumber,
         })
         .where({ id });
-
+      
       return res.status(200).send();
     } catch (error) {
       next(error);
@@ -98,7 +105,7 @@ module.exports = {
 
       const [user] = await knex("users")
         .where({ email })
-        .select("password", "id");
+        .select("password", "id", "confirmed");
 
       if (user === undefined) {
         return res.status(401).send({ error: "Usuário não encontrado" });
@@ -122,6 +129,7 @@ module.exports = {
 
         res.status(201).send({
           id: user.id,
+          confirmed: user.confirmed,
           token,
         });
       } else {
@@ -135,8 +143,12 @@ module.exports = {
   async auth(req, res, next) {
     const { post } = res.locals;
 
+    let [ { confirmed } ] = await knex("users").where({
+      id: post
+    }).select("confirmed");
+
     res
       .status(200)
-      .send({ auth: true, success: "Logado com sucesso!", userID: post });
+      .send({ auth: true, success: "Logado com sucesso!", userID: post, confirmed });
   },
 };
