367e1c3030b37eba8a1b7bd0f4002153d516d6e0
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2021-05-08 11:44:52 -0300

    Removed Loading

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,36 +1,17 @@
-import { useState, useEffect } from "react";
+import React from "react";
 import "./styles.css";
 import { Link } from "react-router-dom";
 
-import Loader from "react-loader-spinner";
 import logo from "../../assets/images/logo.svg";
 import medics from "../../assets/images/medics.svg";
 import { FaCommentMedical, FaClinicMedical } from "react-icons/fa";
 
 const Landing = () => {
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    setIsLoading(true);
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 500);
-  }, []);
-
   return (
     <div id="page-landing">
-      {isLoading ? (
-        <Loader
-          type="Puff"
-          color="#00BFFF"
-          height={100}
-          width={100}
-          timeout={3000}
-        />
-      ) : (
         <div id="page-landing-container" className="container">
           <div className="logo-container">
-            <img src={logo} alt="Spital" />
+            <img src={logo} alt="Spital"/>
             <h2>Sua plataforma para cuidar da saúde e do bolso</h2>
           </div>
 
@@ -56,7 +37,6 @@ const Landing = () => {
             O melhor site para agendar a sua consulta
           </span>
         </div>
-      )}
     </div>
   );
 };
--- web/src/pages/LoginSpitalAccount/index.tsx
+++ web/src/pages/LoginSpitalAccount/index.tsx
@@ -8,27 +8,25 @@ import returnIcon from "../../assets/images/icons/return.svg";
 import logo from "../../assets/images/logo.svg";
 import Loader from "react-loader-spinner";
 import { useAuth } from "../../context/AuthProvider";
+import { useModal } from "../../context/ModalProvider";
 
 const LoginSpitalAccount = () => {
+  const { spinner } = useModal();
   const { login, confirmed } = useAuth();
   const [user, setUser] = useState({
     email: "",
     password: "",
   });
 
-  useEffect(() => {
-    setIsLoading(true);
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 500);
-  }, []);
-
   const [error, setError] = useState("");
 
   async function handleSubmitLogin(e: any) {
     e.preventDefault();
+    spinner.open();
 
     const { error } = await login(user.email, user.password);
+    
+    spinner.close();
     setError(error);
   }
 
@@ -42,98 +40,86 @@ const LoginSpitalAccount = () => {
 
   return (
     <Fragment>
-      {isLoading ? (
-        <Loader
-          type="Puff"
-          color="#00BFFF"
-          height={100}
-          width={100}
-          timeout={3000}
-        />
-      ) : (
-        <Fragment>
-          <div className="header-login">
-            <div className="container header-context">
-              <Link className="return-link" to={`/entrar-paciente`}>
-                <img src={returnIcon} alt="Retornar" className="return" />
-              </Link>
-              <div className="header-title">
-                <h1>
-                  Insira suas informações para que a gente consiga realizar seu
-                  cadastro
-                </h1>
-              </div>
-              <img className="logo" src={logo} alt="Spital" />
+        <div className="header-login">
+          <div className="container header-context">
+            <Link className="return-link" to={`/entrar-paciente`}>
+              <img src={returnIcon} alt="Retornar" className="return" />
+            </Link>
+            <div className="header-title">
+              <h1>
+                Insira suas informações para que a gente consiga realizar seu
+                cadastro
+              </h1>
             </div>
+            <img className="logo" src={logo} alt="Spital" />
           </div>
-          <div className="login-spital-account">
-            <div className="login-spital-account-credentials">
-              <form className="login-spital-account-form">
-                <div className="login-spital-account-form-flex">
-                  <h2>Entre com seu e-mail e senha</h2>
+        </div>
+        <div className="login-spital-account">
+          <div className="login-spital-account-credentials">
+            <form className="login-spital-account-form">
+              <div className="login-spital-account-form-flex">
+                <h2>Entre com seu e-mail e senha</h2>
 
-                  <IconButton
-                    onClick={() => {
-                      handleShowPassword();
-                    }}
-                    color="primary"
-                  >
-                    <span>
-                      {showPassword ? "Esconder senha" : "Mostrar senha"}
-                    </span>
-                  </IconButton>
-                </div>
-                <div className="gray-line"></div>
-
-                <div className="login-spital-account-form-inputs">
-                  <TextField
-                    type="email"
-                    value={user.email}
-                    fullWidth
-                    onChange={(e) => {
-                      setUser({ ...user, email: e.target.value });
-                    }}
-                    variant="outlined"
-                    label={<span style={{ fontSize: "1.5rem" }}>E-mail</span>}
-                    placeholder="Digite seu e-mail"
-                  />
-
-                  <TextField
-                    value={user.password}
-                    style={{ marginTop: "2rem" }}
-                    fullWidth
-                    type={showPassword ? "text" : "password"}
-                    onChange={(e) => {
-                      setUser({ ...user, password: e.target.value });
-                    }}
-                    variant="outlined"
-                    label={<span style={{ fontSize: "1.5rem" }}>Senha</span>}
-                    placeholder="Digite sua senha"
-                  />
-                </div>
-
-                <p
-                  style={{
-                    fontSize: "1.5rem",
-                    fontWeight: "bold",
-                    color: "red",
-                    position: "relative",
-                    top: "-10px",
+                <IconButton
+                  onClick={() => {
+                    handleShowPassword();
                   }}
+                  color="primary"
                 >
-                  {error}
-                </p>
+                  <span>
+                    {showPassword ? "Esconder senha" : "Mostrar senha"}
+                  </span>
+                </IconButton>
+              </div>
+              <div className="gray-line"></div>
 
-                <div className="gray-line" id="gray-line-2"></div>
+              <div className="login-spital-account-form-inputs">
+                <TextField
+                  type="email"
+                  value={user.email}
+                  fullWidth
+                  onChange={(e) => {
+                    setUser({ ...user, email: e.target.value });
+                  }}
+                  variant="outlined"
+                  label={<span style={{ fontSize: "1.5rem" }}>E-mail</span>}
+                  placeholder="Digite seu e-mail"
+                />
 
-                <button onClick={handleSubmitLogin} className="login-button">
-                  Entrar
-                </button>
-              </form>
-            </div>
+                <TextField
+                  value={user.password}
+                  style={{ marginTop: "2rem" }}
+                  fullWidth
+                  type={showPassword ? "text" : "password"}
+                  onChange={(e) => {
+                    setUser({ ...user, password: e.target.value });
+                  }}
+                  variant="outlined"
+                  label={<span style={{ fontSize: "1.5rem" }}>Senha</span>}
+                  placeholder="Digite sua senha"
+                />
+              </div>
+
+              <p
+                style={{
+                  fontSize: "1.5rem",
+                  fontWeight: "bold",
+                  color: "red",
+                  position: "relative",
+                  top: "-10px",
+                }}
+              >
+                {error}
+              </p>
+
+              <div className="gray-line" id="gray-line-2"></div>
+
+              <button onClick={handleSubmitLogin} className="login-button">
+                Entrar
+              </button>
+            </form>
           </div>
-        </Fragment>
-      )}
+        </div>
     </Fragment>
   );
 };
