ba9363ac5625adad1d321bccd398f99a992c1957
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2021-05-08 11:43:29 -0300

    Created Modal-Overlay

---
+++ web/src/components/Modals/Overlay/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './style.css';
+
+interface OverlayProps {
+    children: React.ReactNode;
+}
+
+export default function Overlay({ children }: OverlayProps) {
+    return (
+        <div className="modal-overlay">
+            { children }
+        </div>
+    )
+}
