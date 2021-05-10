eb7c713eb61f06f2020cf5830a5bb816cff72247
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2021-05-10 20:13:42 -0300

    Base TextStyles Created

---
+++ mobile/lib/core/app_text_styles.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'app_colors.dart';
+
+class AppTextStyles {
+  static final TextStyle titleBold = GoogleFonts.quicksand(
+    color: AppColors.black,
+    fontSize: 36,
+    fontWeight: FontWeight.bold
+  );
+
+  static final TextStyle warningTitle = GoogleFonts.quicksand(
+    color: Colors.white,
+    fontSize: 20,
+    fontWeight: FontWeight.bold
+  );
+
+  static final TextStyle warningDescription = GoogleFonts.quicksand(
+    color: Colors.white,
+    fontSize: 16,
+    fontWeight: FontWeight.w500
+  );
+
+  static final TextStyle categoriesTitle = GoogleFonts.roboto(
+    color: AppColors.black,
+    fontSize: 8,
+    fontWeight: FontWeight.w300
+  );
+
+  static final TextStyle topicTitle = GoogleFonts.roboto(
+    color: AppColors.black,
+    fontSize: 20,
+    fontWeight: FontWeight.bold
+  );
+
+  static final TextStyle topicName = GoogleFonts.roboto(
+    color: AppColors.black,
+    fontSize: 16,
+    fontWeight: FontWeight.bold
+  );
+
+  static final TextStyle topicDescription = GoogleFonts.roboto(
+    color: AppColors.lighGray,
+    fontSize: 12,
+    fontWeight: FontWeight.w500
+  );
+
+  static final TextStyle topicDistance = GoogleFonts.roboto(
+    color: AppColors.lighGray,
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+  );
+
+  static final TextStyle topicStar = GoogleFonts.roboto(
+    color: AppColors.black,
+    fontSize: 12,
+    fontWeight: FontWeight.w500
+  );
+
+  static final TextStyle topicTime = GoogleFonts.roboto(
+    color: AppColors.black,
+    fontSize: 12,
+    fontWeight: FontWeight.w500
+  );
+}
