6502b1ca1e7f1c15a100bcf146a43e420de47716
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2021-05-10 20:13:23 -0300

    AppColors added

---
+++ mobile/lib/core/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final Color blue = Color(0xFF07B3D6);
+  static final Color darkBlue = Color(0xFF16697A);
+  static final Color blueTransparent = Color(0xFF16697A).withOpacity(0.2);
+  static final Color orange = Color(0xFFDB6400);
+  static final Color yellow = Color(0xFFFFA62B);
+  static final Color lighGray = Color(0xFFF6F8FF);
+  static final Color black = Color(0xFF333333);
+  static final Color purpleTransparent = Color(0xFF2837B6).withOpacity(0.5);
+}
