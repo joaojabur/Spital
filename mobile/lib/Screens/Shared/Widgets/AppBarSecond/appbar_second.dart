8d8cb4e7ead3617329acf9fbb978c21d034a992b
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2021-05-12 20:47:14 -0300

    create appbar second

---
+++ mobile/lib/Screens/Shared/Widgets/AppBarSecond/appbar_second.dart
@@ -0,0 +1,28 @@
+import 'package:Spital/core/core.dart';
+import 'package:flutter/material.dart';
+
+class AppbarSecundaria extends PreferredSize {
+  final String title;
+
+  AppbarSecundaria({this.title, double width, double height})
+      : super(
+          preferredSize: Size.fromHeight(height * 0.34),
+          child: Container(
+            height: height * 0.1,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(80),
+                ),
+                color: AppColors.blueTransparent),
+            child: Padding(
+              padding: const EdgeInsets.only(top: 17),
+              child: Center(
+                child: Text(
+                  title,
+                  style: AppTextStyles.titleAppBarSecundaria,
+                ),
+              ),
+            ),
+          ),
+        );
+}
