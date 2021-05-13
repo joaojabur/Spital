853e28277624a4ca09081fde0dd3bb9a84bec822
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2021-05-13 18:31:10 -0300

    change file location

--- mobile/lib/Screens/Home/Widget/TabPages/TabPageHome/page_home.dart
+++ mobile/lib/Screens/Home/Widget/TabPages/TabPageHome/page_home.dart
@@ -1,7 +1,7 @@
 import 'package:Spital/Screens/Home/Widget/TabPages/TabPageHome/list_category.dart';
 import 'package:Spital/Screens/Home/Widget/TabPages/TabPageHome/list_doctors.dart';
 
-import 'package:Spital/Screens/Shared/Widgets/AppBarMain/appbar_main.dart';
+import 'package:Spital/Screens/Shared/Widgets/AppBarMain/appbar_main_widget.dart';
 
 import 'package:flutter/material.dart';
 
