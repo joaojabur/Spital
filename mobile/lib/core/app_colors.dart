4f1770bcbc810af1d6642379908ae1d9c949d293
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2021-05-13 18:09:16 -0300

    new color

--- mobile/lib/core/app_colors.dart
+++ mobile/lib/core/app_colors.dart
@@ -8,6 +8,7 @@ class AppColors {
   static final Color orange = Color(0xFFDB6400);
   static final Color yellow = Color(0xFFFFA62B);
   static final Color lighGray = Color(0xFFF6F8FF);
+  static final Color gray = Color(0xFF8c8c8c);
   static final Color black = Color(0xFF333333);
   static final Color purpleTransparent = Color(0xFF2837B6).withOpacity(0.5);
 }
