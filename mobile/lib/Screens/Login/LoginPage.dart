869a34b46fbb26ef1a94cdfefe67132a9fd714bb
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2021-05-14 22:55:48 -0300

    Added LoginPage

--- mobile/lib/Screens/Login/LoginPage.dart
+++ mobile/lib/Screens/Login/LoginPage.dart
@@ -1,4 +1,8 @@
+import 'package:Spital/screens/Shared/Auth/auth_controller.dart';
+import 'package:Spital/screens/Login/controller/controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:provider/provider.dart';
 
 class LoginPage extends StatefulWidget {
   LoginPage({Key? key}) : super(key: key);
@@ -8,10 +12,33 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  late LoginController controller;
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: Text("Login"),
+    controller = LoginController(
+      Provider.of<AuthController>(context)
+    );
+    return Scaffold(
+      body: Observer(builder: (_){
+        if (controller.logged){
+          Navigator.popAndPushNamed(context, '/');
+        }
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              onChanged: controller.changeEmail,
+            ),
+            TextField(
+              onChanged: controller.changePassword,
+            ),
+            ElevatedButton(
+              child: Text("Logar"),
+              onPressed: controller.login,
+            )
+          ],
+        );
+      })
     );
   }
 }
