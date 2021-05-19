35ae17582dfb7291988b2a8c871e37e8dd9b8477
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2021-05-19 08:40:46 -0300

    add icon as a paramater

--- mobile/lib/Screens/Shared/Widgets/AppBarSecond/appbar_second_widget.dart
+++ mobile/lib/Screens/Shared/Widgets/AppBarSecond/appbar_second_widget.dart
@@ -1,10 +1,20 @@
 import 'package:Spital/core/core.dart';
 import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
 
 class AppbarSecundaria extends PreferredSize {
   final String title;
+  final bool topleftIcon;
+  final bool topRightIcon;
+  final IconData iconAddRight;
 
-  AppbarSecundaria({required this.title, double? width, required double height})
+  AppbarSecundaria(
+      {required this.topleftIcon,
+      required this.iconAddRight,
+      required this.topRightIcon,
+      required this.title,
+      required double width,
+      required double height})
       : super(
           preferredSize: Size.fromHeight(height * 0.34),
           child: Container(
@@ -14,14 +24,39 @@ class AppbarSecundaria extends PreferredSize {
                   bottomRight: Radius.circular(80),
                 ),
                 color: AppColors.blueTransparent),
-            child: Padding(
-              padding: const EdgeInsets.only(top: 17),
-              child: Center(
-                child: Text(
-                  title,
-                  style: AppTextStyles.titleAppBarSecundaria,
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: height * 0.03),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        child: topRightIcon
+                            ? IconButton(
+                                icon: Icon(Ionicons.arrow_back),
+                                onPressed: () {},
+                              )
+                            : null,
+                      ),
+                      Center(
+                        child: Text(
+                          title,
+                          style: AppTextStyles.titleAppBarSecundaria,
+                        ),
+                      ),
+                      Container(
+                        child: topleftIcon
+                            ? IconButton(
+                                icon: (Icon(iconAddRight)),
+                                onPressed: () {},
+                              )
+                            : null,
+                      ),
+                    ],
+                  ),
                 ),
-              ),
+              ],
             ),
           ),
         );
