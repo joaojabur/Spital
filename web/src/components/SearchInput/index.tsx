1fa92ec3ecfa6159b0dcebe176013801643b8190
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2021-05-22 08:33:55 -0300

    Added Filter by categories' name

--- web/src/components/SearchInput/index.tsx
+++ web/src/components/SearchInput/index.tsx
@@ -2,19 +2,18 @@ import React from "react";
 import "./styles.css";
 import { IoSearchOutline } from "react-icons/io5";
 
-interface SearchInputProps {
+interface SearchInputProps extends React.HTMLAttributes<HTMLInputElement> {
   placeholder: string;
 }
 
-const SearchInput: React.FC<SearchInputProps> = ({ placeholder }) => {
+const SearchInput: React.FC<SearchInputProps> = ({ placeholder, ...rest }) => {
   return (
     <div className="search-input">
       <IoSearchOutline
         style={{ padding: "0.2rem", marginLeft: "1rem" }}
         size={30}
-        color="#093D48"
-      />
-      <input type="text" name="search" placeholder={placeholder} />
+        color="#093D48"/>
+      <input type="text" name="search" placeholder={placeholder} {...rest}/>
     </div>
   );
 };
