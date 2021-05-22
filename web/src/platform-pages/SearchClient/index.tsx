dfc4db8d2665619d61fc69744c5a6af34f92cf5e
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2021-05-22 08:37:38 -0300

    search categories working properly

--- web/src/platform-pages/SearchClient/index.tsx
+++ web/src/platform-pages/SearchClient/index.tsx
@@ -1,15 +1,19 @@
+import React, { useState } from "react";
 import HeaderPlatform from "../../components/HeaderPlatform";
 import ListMedicAreas from "../../components/ListMedicAreas";
 import SearchInput from "../../components/SearchInput";
 import "./styles.css";
 
 const SearchClient = () => {
+  const [ searchTerm, setSearchTerm ] = useState('');
   return (
     <div className="client-platform">
       <HeaderPlatform title="Encontre os melhores médicos da sua região" />
       <div className="container">
-        <SearchInput placeholder="Busque pelo área ou nome do médico..." />
-        <ListMedicAreas />
+        <SearchInput 
+          placeholder="Busque pelo área ou nome do médico..."
+          onChange={(e: React.FormEvent<HTMLInputElement>) => setSearchTerm(e.currentTarget.value)} />
+        <ListMedicAreas searchTerm={searchTerm}/>
       </div>
     </div>
   );
