93e6222e9433670c3b3ea54e24db55cd76e71330
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2021-05-22 17:21:18 -0300

    added Distance

--- web/src/components/DoctorItem/index.tsx
+++ web/src/components/DoctorItem/index.tsx
@@ -3,14 +3,17 @@ import { IoStar, IoTimeOutline } from "react-icons/io5";
 import { Link } from "react-router-dom";
 
 export interface DoctorItemProps {
-  medic: {
-    firstName: string;
-    email: number;
-    phoneNumber: string;
-    lastName: string;
-    area: string;
-    userID: number;
-  };
+  medic: Medic;
+}
+
+interface Medic {
+  firstName: string;
+  email: number;
+  phoneNumber: string;
+  lastName: string;
+  area: string;
+  userID: number;
+  distance: number;
 }
 
 const DoctorItem = ({ medic }: DoctorItemProps) => {
@@ -19,7 +22,9 @@ const DoctorItem = ({ medic }: DoctorItemProps) => {
   return (
     <Link to={`/medicos/${id}`} className="doctor-item">
       <div className="doctor-item-first">
-        <div className="doctor-item-image"></div>
+        <div className="doctor-item-image">
+          <img src={`https://avatars.dicebear.com/api/human/${medic.firstName + medic.lastName}.svg`} alt="Medic Profile"/>
+        </div>
         <div className="doctor-item-data">
           <h1>Dr. {medic.firstName}</h1>
           <span>{medic.area}</span>
@@ -29,7 +34,11 @@ const DoctorItem = ({ medic }: DoctorItemProps) => {
           </div>
         </div>
         <div className="doctor-item-data-location">
-          <p className="doctor-item-data-distance">1.5 km</p>
+          { medic.distance &&
+            <p className="doctor-item-data-distance">{
+              medic.distance.toFixed(1)
+            } km</p>
+          }
         </div>
       </div>
     </Link>
