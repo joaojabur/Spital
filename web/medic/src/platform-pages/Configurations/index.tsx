a7de4b24baf0897824531e90e9076ef3f3623cd9
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2021-06-12 18:02:00 -0300

    Created  Configuration page

--- web/medic/src/platform-pages/Configurations/index.tsx
+++ web/medic/src/platform-pages/Configurations/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useHistory } from "react-router";
 import HorizontalHeader from "../../components-platform/HorizontalHeader";
 import VerticalHeader from "../../components-platform/VerticalHeader";
+import ConfigContainer from "../../components/ConfigContainer";
 import { useAuth } from "../../context/AuthProvider";
 import "./styles.css";
 
@@ -12,10 +13,39 @@ const Configurations = () => {
   if (!user.configured) {
     history.replace("/configurar");
   }
+
+  console.log(user)
   return (
-    <div className="agenda">
+    <div className="config">
       <HorizontalHeader title="Configurações" />
       <VerticalHeader colorIcon="configurations" />
+      <div className="content">
+        <ConfigContainer>
+          <h3>Dados da Clínica</h3>
+          <div className="data">
+            <p><strong>Endereço:</strong>{user.location.address.replace("State of", '')}</p>
+            <p><strong>Número:</strong>{user.location.number}</p>
+          </div>
+        </ConfigContainer>
+        <ConfigContainer>
+          <h3>Dados Bancários</h3>
+          <div className="data">
+            <p><strong>Endereço:</strong></p>
+            <p><strong>Endereço:</strong></p>
+            <p><strong>Endereço:</strong></p>
+            <p><strong>Endereço:</strong></p>
+          </div>
+        </ConfigContainer>
+        <ConfigContainer>
+          <h3>Dados de Consulta</h3>
+          <div className="data">
+            <p><strong>Endereço:</strong></p>
+            <p><strong>Endereço:</strong></p>
+            <p><strong>Endereço:</strong></p>
+            <p><strong>Endereço:</strong></p>
+          </div>
+        </ConfigContainer>
+      </div>
     </div>
   );
 };
