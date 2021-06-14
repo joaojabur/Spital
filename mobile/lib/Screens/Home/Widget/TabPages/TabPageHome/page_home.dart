ea48c642ae9ea7b7a5f3d2ba1fa532c366ec64e2
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2021-06-14 17:24:32 -0300

    just formatting

--- mobile/lib/Screens/Home/Widget/TabPages/TabPageHome/page_home.dart
+++ mobile/lib/Screens/Home/Widget/TabPages/TabPageHome/page_home.dart
@@ -3,7 +3,7 @@ import 'package:Spital/Screens/Home/Widget/TabPages/TabPageHome/Widgets/list_doc
 import 'package:Spital/Screens/Home/Widget/TabPages/TabPageHome/controller/page_home_controller.dart';
 
 import 'package:Spital/Screens/Shared/Widgets/AppBarMain/appbar_main_widget.dart';
-import 'package:Spital/screens/Shared/Auth/auth_controller.dart';
+import 'package:Spital/Screens/Shared/Auth/auth_controller.dart';
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -23,11 +23,10 @@ class _PageHomeState extends State<PageHome> {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppbarWidget(
-        width: width,
-        height: height,
-        name: pageHomeController.getUserFirstName(),
-        image: pageHomeController.getUserImage()
-      ),
+          width: width,
+          height: height,
+          name: pageHomeController.getUserFirstName(),
+          image: pageHomeController.getUserImage()),
       body: Container(
         height: height,
         child: Column(
