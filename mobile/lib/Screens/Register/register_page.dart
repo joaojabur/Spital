7c9474ea2df9a67e3b14c8336a61fda0b4f7a0e9
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2021-06-14 17:27:49 -0300

    code optimization

--- mobile/lib/Screens/Register/register_page.dart
+++ mobile/lib/Screens/Register/register_page.dart
@@ -1,101 +1,57 @@
+import 'package:Spital/Screens/Register/TabRegister/TabRegisterFifth/tab_page_fifth.dart';
+import 'package:Spital/Screens/Register/TabRegister/TabRegisterFirst/page_register_first.dart';
+import 'package:Spital/Screens/Register/TabRegister/TabRegisterFourth/tab_page_fourth.dart';
+import 'package:Spital/Screens/Register/TabRegister/TabRegisterThird/tab_page_third.dart';
+import 'package:Spital/Screens/Register/controller/register_controller.dart';
 import 'package:Spital/Screens/Shared/Widgets/AppBarSliver/sliver_appbar.dart';
-import 'package:Spital/core/app_colors.dart';
-import 'package:Spital/core/app_text_styles.dart';
 import 'package:flutter/material.dart';
-
 import 'package:ionicons/ionicons.dart';
+import 'package:provider/provider.dart';
+import 'TabRegister/TabRegisterSecond/page_register_second.dart';
 
 class RegisterPage extends StatefulWidget {
   @override
   _RegisterPageState createState() => _RegisterPageState();
 }
 
-class _RegisterPageState extends State<RegisterPage> {
+class _RegisterPageState extends State<RegisterPage>
+    with SingleTickerProviderStateMixin {
+  late RegisterController register_controller;
+
   @override
+  void initState() {
+    super.initState();
+    register_controller = RegisterController(length: 5, tickerProvider: this);
+  }
+
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-    double height = MediaQuery.of(context).size.height;
-    return Scaffold(
-      resizeToAvoidBottomInset: true,
-      body: NestedScrollView(
-        headerSliverBuilder: (context, condition) {
-          return <Widget>[
-            SliverPage(
+    return MultiProvider(
+      providers: [
+        Provider<RegisterController>(create: (_) => register_controller)
+      ],
+      child: Scaffold(
+        resizeToAvoidBottomInset: true,
+        body: NestedScrollView(
+          headerSliverBuilder: (context, condition) {
+            return <Widget>[
+              AppBarSliverPage(
                 topleftIcon: false,
-                iconAddRight: Ionicons.chevron_back_outline,
-                topRightIcon: false,
-                title: "Resgistrar",
-                height: height,
-                width: width)
-          ];
-        },
-        body: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 100),
-                child: Container(
-                  margin: EdgeInsets.only(right: 80, bottom: 30),
-                  child: Text(
-                    "1.Quem é você",
-                    style: AppTextStyles.titleBold,
-                  ),
-                ),
-              ),
-              Container(
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
-                  child: TextField(
-                    decoration: InputDecoration(
-                        labelText: 'Email',
-                        labelStyle: TextStyle(color: AppColors.gray),
-                        border: OutlineInputBorder(),
-                        focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: AppColors.blue)),
-                        errorBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: AppColors.blue)),
-                        enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: AppColors.blue)),
-                        disabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: AppColors.blue))),
-                    //controller:
-                  )),
-              Container(
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
-                  child: TextField(
-                    decoration: InputDecoration(
-                        labelText: 'Senha',
-                        labelStyle: TextStyle(color: AppColors.gray),
-                        border: OutlineInputBorder(),
-                        focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: AppColors.blue)),
-                        errorBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: AppColors.blue)),
-                        enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: AppColors.blue)),
-                        disabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: AppColors.blue))),
-                    //controller:
-                  )),
-              Container(
-                margin: EdgeInsets.only(
-                  top: height * 0.05,
-                ),
-                padding: EdgeInsets.symmetric(
-                  horizontal: width * 0.27,
-                ),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    color: AppColors.blueT100),
-                child: TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    "Próximo",
-                    style: AppTextStyles.warningTitle,
-                  ),
-                ),
+                title: "Crie aqui\n sua conta!",
+                iconLeft: Ionicons.chevron_back_outline,
+                image: "images/register.svg",
               )
+            ];
+          },
+          body: TabBarView(
+            physics: NeverScrollableScrollPhysics(),
+            children: <Widget>[
+              PageRegisterFirst(),
+              PageRegisterSecond(),
+              PageRegisterThird(),
+              PageRegisterFourth(),
+              PageRegisterFifth(),
             ],
+            controller: register_controller.tabRegisterController,
           ),
         ),
       ),
