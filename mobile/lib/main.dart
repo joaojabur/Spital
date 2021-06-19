5f0a07625372aaea40b19b94fa7e5a22a4a8dc38
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2021-06-19 17:16:04 -0300

    add cache

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,6 +1,22 @@
 import 'package:Spital/core/app_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svg/svg.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  Future.wait([
+    precachePicture(
+      ExactAssetPicture(SvgPicture.svgStringDecoder, "images/splash.svg"),
+      null,
+    ),
+    precachePicture(
+      ExactAssetPicture(
+          SvgPicture.svgStringDecoder, "images/ilustracao_login.svg"),
+      null,
+    ),
+  ]);
+
   runApp(AppWidget());
 }
