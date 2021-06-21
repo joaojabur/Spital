57eeb9196a88eba8a65180ef93cd0ca85921e7c7
CarlosYanBezerraVieira <80974859+CarlosYanBezerraVieira@users.noreply.github.com>
2021-06-21 08:06:42 -0300

    adding parameters

--- mobile/lib/Screens/Shared/Widgets/AppBarSliver/sliver_appbar.dart
+++ mobile/lib/Screens/Shared/Widgets/AppBarSliver/sliver_appbar.dart
@@ -1,22 +1,28 @@
 import 'package:Spital/Screens/Register/controller/register_controller.dart';
+import 'package:Spital/Screens/Shared/Widgets/AppBarSliver/controller/controller.dart';
 import 'package:Spital/core/app_colors.dart';
 import 'package:Spital/core/app_text_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:provider/provider.dart';
 
 class AppBarSliverPage extends StatefulWidget {
-  const AppBarSliverPage({
-    Key? key,
-    required this.title,
-    required this.topleftIcon,
-    required this.iconLeft,
-    required this.image,
-  }) : super(key: key);
+  const AppBarSliverPage(
+      {Key? key,
+      required this.title,
+      required this.text,
+      required this.topleftIcon,
+      required this.iconLeft,
+      required this.image,
+      required this.onBackPressed})
+      : super(key: key);
 
   final String title;
+  final String text;
   final String image;
+  final Function() onBackPressed;
 
   final bool topleftIcon;
 
@@ -27,57 +33,61 @@ class AppBarSliverPage extends StatefulWidget {
 }
 
 class _AppBarSliverPageState extends State<AppBarSliverPage> {
+  final SliverAppBarController controller = SliverAppBarController();
   @override
   Widget build(BuildContext context) {
-    RegisterController controller = Provider.of<RegisterController>(context);
-    double width = MediaQuery.of(context).size.width;
-    double height = MediaQuery.of(context).size.height;
     return SliverAppBar(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.only(bottomRight: Radius.circular(40)),
-      ),
-      backgroundColor: AppColors.blueTransparent,
-      expandedHeight: 200,
-      pinned: true,
-      floating: true,
-      centerTitle: true,
-      leading: Container(
-        margin: EdgeInsets.only(left: 15, right: 0, top: 5, bottom: 12),
-        width: 50,
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10), color: Colors.white),
-        child: Padding(
-          padding: EdgeInsets.only(right: 15),
-          child: IconButton(
-              icon: Icon(widget.iconLeft),
-              color: AppColors.darkBlue,
-              onPressed: () {
-                controller.tabRegisterIndex == 0
-                    ? Navigator.pop(context)
-                    : controller
-                        .changePageRegister(controller.tabRegisterIndex! - 1);
-              }),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(bottomRight: Radius.circular(40)),
         ),
-      ),
-      flexibleSpace: FlexibleSpaceBar(
-        background: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: EdgeInsets.only(top: 30, left: 10),
-              child: Center(
-                child: Text(widget.title, style: AppTextStyles.titleBold2),
-              ),
-            ),
-            Container(
-                width: 200,
-                child:
-                    widget.image == "" ? null : SvgPicture.asset(widget.image)),
-          ],
-        ),
-        // title: Text(title),
+        backgroundColor: AppColors.blueTransparent,
+        expandedHeight: 200,
+        pinned: true,
+        floating: true,
         centerTitle: true,
-      ),
-    );
+        shadowColor: Colors.transparent,
+        title: Observer(builder: (_) {
+          return Text(
+            controller.isExpanded ? '' : widget.text,
+            style: AppTextStyles.titleBold2,
+          );
+        }),
+        leading: Container(
+          margin: EdgeInsets.only(left: 15, right: 0, top: 5, bottom: 12),
+          width: 50,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10), color: Colors.white),
+          child: IconButton(
+              icon: Padding(
+                padding: EdgeInsets.only(right: 15),
+                child: Icon(widget.iconLeft),
+              ),
+              color: AppColors.darkBlue,
+              onPressed: this.widget.onBackPressed),
+        ),
+        flexibleSpace: LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+          SchedulerBinding.instance!.addPostFrameCallback((_) {
+            controller.changeExpanded((constraints.biggest.height > 100));
+          });
+
+          return FlexibleSpaceBar(
+              background: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 30, left: 10),
+                child: Center(
+                  child: Text(widget.title, style: AppTextStyles.titleBold2),
+                ),
+              ),
+              Container(
+                  width: widget.image == "" ? 0 : 200,
+                  child: widget.image == ""
+                      ? null
+                      : SvgPicture.asset(widget.image)),
+            ],
+          ));
+        }));
   }
 }
