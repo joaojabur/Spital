99a2a7d2d85866ec491f34734607f556ca3b8b04
João Jabur <45864544+joaojabur@users.noreply.github.com>
2021-06-29 14:51:32 -0300

    getting build back again

--- web/client/src/services/api.ts
+++ web/client/src/services/api.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:3333/";
+const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "54.207.108.57"
+    : "http://localhost:3333/";
 
 const api = axios.create({
   baseURL,
