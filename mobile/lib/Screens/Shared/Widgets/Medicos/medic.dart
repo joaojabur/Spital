5ea67d9cce1da1cdac16d9d2ef6ba46425fcad4b
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2021-07-03 09:42:17 -0300

    extension of medicModel with data

---
+++ mobile/lib/Screens/Shared/Widgets/Medicos/medic.dart
@@ -0,0 +1,17 @@
+import 'package:Spital/Screens/Shared/Models/medic_model.dart';
+
+class Medic {
+  MedicModel medicModel = MedicModel(
+      area: 'Cardiologia',
+      birthDate: '12/10/1990',
+      cpf: '12345678900',
+      crm: 'A054008',
+      doctorateDegree: 'Doutorados Medicina.',
+      email: 'Medico@gmail.com',
+      firstName: 'Pedro',
+      graduation: 'Cardiologia',
+      lastName: 'Artur',
+      masterDegree: 'Medicina',
+      phoneNumber: '12345678900',
+      rg: '123456789');
+}
