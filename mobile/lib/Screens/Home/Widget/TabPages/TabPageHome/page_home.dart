c7bf2f3c18ae9efede17fa22c2fd0e2e3f7a54f6
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2021-07-08 22:36:17 -0300

    Get location data

--- mobile/lib/Screens/Home/Widget/TabPages/TabPageHome/page_home.dart
+++ mobile/lib/Screens/Home/Widget/TabPages/TabPageHome/page_home.dart
@@ -1,12 +1,14 @@
 import 'package:Spital/Screens/Home/Widget/TabPages/TabPageHome/Widgets/list_category.dart';
 import 'package:Spital/Screens/Home/Widget/TabPages/TabPageHome/Widgets/list_doctors.dart';
 import 'package:Spital/Screens/Home/Widget/TabPages/TabPageHome/controller/page_home_controller.dart';
+import 'package:Spital/Screens/Shared/controllers/location_controller.dart';
 
 import 'package:Spital/Screens/Shared/Widgets/AppBarMain/appbar_main_widget.dart';
 import 'package:Spital/Screens/Shared/Auth/auth_controller.dart';
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:geolocator/geolocator.dart';
 
 class PageHome extends StatefulWidget {
   @override
@@ -15,11 +17,40 @@ class PageHome extends StatefulWidget {
 
 class _PageHomeState extends State<PageHome> {
   PageHomeController pageHomeController = PageHomeController();
+
+  getLocationPermission(LocationController controller) async {
+    bool serviceEnabled;
+    LocationPermission permission;
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+    return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      return Future.error('Location permissions are denied');
+    }
+    }
+
+    if (permission == LocationPermission.deniedForever) { 
+    return Future.error(
+      'Location permissions are permanently denied, we cannot request permissions.');
+    } 
+
+    controller.setPosition(await Geolocator.getCurrentPosition());
+  }
+
   @override
   Widget build(BuildContext context) {
     pageHomeController.authController = Provider.of<AuthController>(context);
+    LocationController location = Provider.of<LocationController>(context);
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
+
+   getLocationPermission(location);
+    
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppbarWidget(
