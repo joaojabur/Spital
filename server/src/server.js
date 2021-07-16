a3d54f8bf0874d318e8a15e633efe0aa941a811a
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2021-07-16 07:33:24 -0300

    added urlenconded type for file upload

--- server/src/server.js
+++ server/src/server.js
@@ -17,6 +17,7 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(cookieParser());
+app.use(express.urlencoded({ extended: true}));
 
 app.use((req, res, next) => {
   const error = new Error("Not Found");
