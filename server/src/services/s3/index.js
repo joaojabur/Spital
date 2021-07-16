31b6b85251aeb369862bee34ecac7406146cb2a5
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2021-07-16 07:33:56 -0300

    created services file for AWS s3

---
+++ server/src/services/s3/index.js
@@ -0,0 +1,28 @@
+require('dotenv').config({ path: "../.env" });
+const AWS = require('aws-sdk');
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_S3_ACESSKEY,
+  secretAccessKey: process.env.AWS_S3_SECRET_ACESS,
+  region: process.env.AWS_DEFAULT_REGION
+});
+
+var s3 = new AWS.S3();
+
+module.exports = {
+    uploadS3({filename, bucket, data}){
+        return new Promise((resolve, reject) => {
+          s3.putObject({
+            Key: filename,
+            Bucket: bucket,
+            Body: data
+          }, (err, data) => {
+            if (err){
+              reject(err)
+            } else {
+              resolve(data)
+            }
+          });
+        })
+    }    
+}
