2b1992cef7bd7d9b5f3492174665cb1dabf5050f
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2021-07-19 10:57:56 -0300

    Created ReviewPage Controller

---
+++ mobile/lib/Screens/Review/controller/review_controller.dart
@@ -0,0 +1,25 @@
+import 'package:Spital/Screens/Review/controller/review_repository.dart';
+import 'package:Spital/Screens/Shared/Models/review_model.dart';
+import 'package:mobx/mobx.dart';
+part 'review_controller.g.dart';
+
+class ReviewController = _ReviewControllerBase with _$ReviewController;
+
+abstract class _ReviewControllerBase with Store {
+  ReviewRepository repository = ReviewRepository();
+
+  @observable
+  ObservableList<ReviewModel> reviews = ObservableList<ReviewModel>();
+
+  Future<String> loadReviews(int medicID) async {
+    ResponseReview response = await repository.loadReviews(medicID);
+
+    if (response.error){
+      return response.message!;
+    }
+
+    reviews = reviews.asObservable();
+
+    return "";
+  }
+}
