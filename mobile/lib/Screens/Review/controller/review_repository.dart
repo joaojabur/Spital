ff84a5dc61da0175710a32297f5e90fc90572ce5
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2021-07-21 09:43:19 -0300

    I left medicID as static equal to 1

--- mobile/lib/Screens/Review/controller/review_repository.dart
+++ mobile/lib/Screens/Review/controller/review_repository.dart
@@ -1,4 +1,3 @@
-
 import 'package:Spital/Screens/Shared/Models/review_model.dart';
 import 'package:Spital/Screens/Shared/services/dio_instance.dart';
 import 'package:dio/dio.dart';
@@ -17,22 +16,20 @@ class ReviewRepository {
   Future<ResponseReview> loadReviews(int medicID) async {
     try {
       List<ReviewModel> reviews = [];
-      final response = await dio.get(
-        '/reviews?medicID=$medicID',
-        options: Options(
+      final response = await dio.get('/reviews?medicID=1',
+          options: Options(
             followRedirects: false,
             validateStatus: (status) {
               return status! < 500;
             },
-          )
-        );
+          ));
 
       for (var review in List.from(response.data)) {
         reviews.add(ReviewModel.fromMap(review));
       }
 
       return ResponseReview(error: false, reviews: reviews);
-    } catch(err){
+    } catch (err) {
       return ResponseReview(error: true, message: "Ocorreu um erro.");
     }
   }
