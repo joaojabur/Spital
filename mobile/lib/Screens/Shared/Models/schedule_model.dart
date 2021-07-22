87ded1b1c2d36a93406a7fdfa1fcaec986dce9dd
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2021-07-22 07:39:35 -0300

    Created ScheduleModel

---
+++ mobile/lib/Screens/Shared/Models/schedule_model.dart
@@ -0,0 +1,61 @@
+import 'dart:convert';
+
+class ScheduleModel {
+  final String from;
+  final String to;
+  final String weekDay;
+
+  ScheduleModel(
+    this.from,
+    this.to,
+    this.weekDay,
+  );
+
+  ScheduleModel copyWith({
+    String? from,
+    String? to,
+    String? weekDay,
+  }) {
+    return ScheduleModel(
+      from ?? this.from,
+      to ?? this.to,
+      weekDay ?? this.weekDay,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'from': from,
+      'to': to,
+      'weekDay': weekDay,
+    };
+  }
+
+  factory ScheduleModel.fromMap(Map<String, dynamic> map) {
+    return ScheduleModel(
+      map['from'],
+      map['to'],
+      map['week_day'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory ScheduleModel.fromJson(String source) => ScheduleModel.fromMap(json.decode(source));
+
+  @override
+  String toString() => 'ScheduleModel(from: $from, to: $to, weekDay: $weekDay)';
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+  
+    return other is ScheduleModel &&
+      other.from == from &&
+      other.to == to &&
+      other.weekDay == weekDay;
+  }
+
+  @override
+  int get hashCode => from.hashCode ^ to.hashCode ^ weekDay.hashCode;
+}
