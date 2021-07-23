c6efd2886b7bc9ad8a144bc908b1c0ba6de65df6
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2021-07-22 22:41:06 -0300

    Added Calendar

--- mobile/lib/Screens/Schedule/SchedulePage.dart
+++ mobile/lib/Screens/Schedule/SchedulePage.dart
@@ -1,4 +1,7 @@
+import 'package:Spital/Screens/Schedule/controllers/schedule_page_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class SchedulePage extends StatefulWidget {
   SchedulePage({Key? key}) : super(key: key);
@@ -8,12 +11,38 @@ class SchedulePage extends StatefulWidget {
 }
 
 class _SchedulePageState extends State<SchedulePage> {
+  SchedulePageController controller = SchedulePageController();
   @override
   Widget build(BuildContext context) {
     final medicID = ModalRoute.of(context)!.settings.arguments as int;
 
     return Scaffold(
-      body: Container()
+      body: SafeArea(
+        child: Observer(
+          builder: (context){
+            return  Column(
+              children: [
+                TableCalendar(
+                  firstDay: DateTime.utc(2021, 1, 1),
+                  lastDay: DateTime.utc(2030, 3, 14),
+                  focusedDay: DateTime.now(),
+                  onDaySelected: (selectedDay, _){
+                    if (!isSameDay(controller.focusedDay, selectedDay)){
+                      controller.changeFocusedDay(selectedDay);
+                    }
+                  },
+                  selectedDayPredicate: (day) {
+                    return isSameDay(controller.focusedDay, day);
+                  },
+                  onPageChanged: (focusedDay) {
+                    controller.changeFocusedDay(focusedDay);
+                  },
+                )
+              ],
+            );
+          }
+        )
+      )
     );
   }
 }
