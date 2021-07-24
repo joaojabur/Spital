ce3d3f5056982c8d12c5c700860ea1d2d76c674e
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2021-07-24 13:08:52 -0300

    corrected loadSchedule, add scheduleToTimeRange

--- mobile/lib/Screens/Schedule/controllers/schedule_page_controller.dart
+++ mobile/lib/Screens/Schedule/controllers/schedule_page_controller.dart
@@ -29,13 +29,13 @@ abstract class _SchedulePageControllerBase with Store {
     focusedDay = value;
   }
 
-  Future<String> loadSchedule(int medicID, WeekDay weekDay) async {
-    final response = await repository.loadSchedule(medicID, weekDay);
+  Future<String> loadSchedule(int medicID) async {
+    final response = await repository.loadSchedule(medicID);
 
     if (response.error) {
       return response.message!;
     }
-
+    
     medicSchedule = response.schedule!.asObservable();
 
     return '';
@@ -65,8 +65,49 @@ abstract class _SchedulePageControllerBase with Store {
     return '';
   }
 
-  Future<void> loadInitialData(int medicID, WeekDay weekDay) async {
-    await loadSchedule(medicID, weekDay);
+  Future<void> loadInitialData(int medicID) async {
+    await loadSchedule(medicID);
     await loadConsultTypes(medicID);
+  }
+
+  formatTime() {
+    String day = focusedDay.day.toString();
+    String month = focusedDay.month.toString();
+    String year = focusedDay.year.toString();
+    String dataFormatted = "${month}/${day}/${year}";
+    
+    return dataFormatted;
+  }
+
+  List<String> scheduleToTimeRange(ScheduleModel schedule) {
+    List<String> hours = [];
+
+    int initialHour = int.parse(schedule.from) ~/ 60;
+    int initialMinutes = int.parse(schedule.from) % 60;
+    int lastHour =  int.parse(schedule.to) ~/ 60;
+    int lastMinutes = int.parse(schedule.to) % 60;
+    int rangeSize = ((int.parse(schedule.to) - int.parse(schedule.from)) / 60 * 2 - 1).toInt();
+    print('range: $rangeSize');
+
+    hours.add('$initialHour:${initialMinutes.toString().padLeft(2, '0')}');
+    for (int i = 0; i < rangeSize; i++){
+      int currentTime = int.parse(schedule.from) + 30 * (i + 1);
+      int currentHour = currentTime ~/ 60;
+      int currentMinutes = currentTime % 60;
+
+      hours.add('$currentHour:${currentMinutes.toString().padLeft(2, '0')}');
+    }
+    hours.add('$lastHour:${lastMinutes.toString().padLeft(2, '0')}');
+    return hours;
+  }
+
+  getWeekDay(){
+    int weekDay = focusedDay.weekday;
+    
+    if (weekDay == 7){
+      return 0;
+    }
+
+    return weekDay;
   }
 }
