5f10b7b3ec24f8e2ee3daca825fec7179318f22b
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2021-07-27 07:51:12 -0300

    Added SingleChildScrollView

--- mobile/lib/Screens/Schedule/SchedulePage.dart
+++ mobile/lib/Screens/Schedule/SchedulePage.dart
@@ -43,166 +43,179 @@ class _SchedulePageState extends State<SchedulePage> {
             builder: (context, snapshot) {
               if (snapshot.connectionState == ConnectionState.done) {
                 return Observer(builder: (context) {
-                  return Column(
-                    children: [
-                      TableCalendar(
-                        firstDay: DateTime.utc(2021, 1, 1),
-                        lastDay: DateTime.utc(2030, 3, 14),
-                        focusedDay: controller.focusedDay,
-                        onDaySelected: (selectedDay, _) {
-                          if (!isSameDay(controller.focusedDay, selectedDay)) {
-                            controller.changeFocusedDay(selectedDay);
-                          }
-                        },
-                        selectedDayPredicate: (day) {
-                          return isSameDay(controller.focusedDay, day);
-                        },
-                        onPageChanged: (focusedDay) {
-                          controller.changeFocusedDay(focusedDay);
-                        },
-                      ),
-                      FutureBuilder(
-                        future: controller.loadCurrentAppointment(
-                            medicID, controller.formatTime()),
-                        builder: (context, snapshot) {
-                          if (snapshot.connectionState ==
-                              ConnectionState.done) {
-                            print(controller.medicSchedule);
-                            print(controller.currentAppointment);
-                            try {
-                              ScheduleModel currentSchedule = controller
-                                  .medicSchedule
-                                  .firstWhere((element) =>
-                                      element.weekDay ==
-                                      controller.getWeekDay());
-
-                              List<String> timeRange = controller
-                                  .scheduleToTimeRange(currentSchedule);
-                              String date = controller.formatTime();
-                              return Flexible(
-                                child: Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 10, vertical: 15),
-                                  child: GridView.builder(
-                                    physics: BouncingScrollPhysics(),
-                                    gridDelegate:
-                                        SliverGridDelegateWithFixedCrossAxisCount(
-                                            crossAxisCount: 2,
-                                            childAspectRatio: 4.5,
-                                            crossAxisSpacing: 10,
-                                            mainAxisSpacing: 20),
-                                    itemCount: timeRange.length,
-                                    itemBuilder: (context, index) {
-                                      String range = timeRange[index];
-                                      bool isReserved = false;
-                                      print(controller.currentAppointment);
-                                      print("controller.currentAppointment");
-                                      try {
-                                        var reserve = controller
-                                            .currentAppointment
-                                            .firstWhere((element) =>
-                                                element.time == range &&
-                                                element.date == date);
-                                        isReserved = true;
-                                      } catch (err) {}
-
-                                      return Container(
-                                          child: TimeButton(
-                                        text: range,
-                                        onPressed: () {
-                                          controller.changedSelectedTime(range);
-                                        },
-                                        reserved: isReserved,
-                                        selected:
-                                            controller.selectedTime == range,
-                                      ));
-                                    },
-                                  ),
-                                ),
-                              );
-                            } catch (err) {
-                              return Text("Não possui consultas nesse dia");
+                  return SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        TableCalendar(
+                          firstDay: DateTime.utc(2021, 1, 1),
+                          lastDay: DateTime.utc(2030, 3, 14),
+                          focusedDay: controller.focusedDay,
+                          onDaySelected: (selectedDay, _) {
+                            if (!isSameDay(controller.focusedDay, selectedDay)) {
+                              controller.changeFocusedDay(selectedDay);
                             }
-                          }
+                          },
+                          selectedDayPredicate: (day) {
+                            return isSameDay(controller.focusedDay, day);
+                          },
+                          onPageChanged: (focusedDay) {
+                            controller.changeFocusedDay(focusedDay);
+                          },
+                        ),
+                        FutureBuilder(
+                          future: controller.loadCurrentAppointment(
+                              medicID, controller.formatTime()),
+                          builder: (context, snapshot) {
+                            if (snapshot.connectionState ==
+                                ConnectionState.done) {
+                              print(controller.medicSchedule);
+                              print(controller.currentAppointment);
+                              try {
+                                ScheduleModel currentSchedule = controller
+                                    .medicSchedule
+                                    .firstWhere((element) =>
+                                        element.weekDay ==
+                                        controller.getWeekDay());
 
-                          return Center(child: CircularProgressIndicator());
-                        },
-                      ),
-                      Flexible(
-                        child: ListView.builder(
-                            itemCount: controller.consultsType.length,
-                            itemBuilder: (context, index) {
-                              ConsultModel consult =
-                                  controller.consultsType[index];
-                              return Padding(
-                                padding: const EdgeInsets.all(15),
-                                child: PhysicalModel(
-                                  color: Colors.black,
-                                  elevation: 2,
-                                  borderRadius: BorderRadius.circular(10),
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                        color: Colors.white,
-                                        borderRadius:
-                                            BorderRadius.circular(10)),
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(10),
-                                      child: Column(
-                                        children: [
-                                          ElevatedButton(
-                                              style: ButtonStyle(
-                                                  backgroundColor:
-                                                      MaterialStateProperty.all<
-                                                              Color>(
-                                                          AppColors.darkBlue),
-                                                  shape: MaterialStateProperty
-                                                      .all<RoundedRectangleBorder>(
-                                                          RoundedRectangleBorder(
-                                                    borderRadius:
-                                                        BorderRadius.circular(
-                                                            10.0),
-                                                  ))),
+                                List<String> timeRange = controller
+                                    .scheduleToTimeRange(currentSchedule);
+                                String date = controller.formatTime();
+                                return Column(
+                                  children: [
+                                    Text("Horários Disponíveis"),
+                                    ConstrainedBox(
+                                      constraints: BoxConstraints(
+                                        maxHeight: timeRange.length * 31
+                                      ),
+                                      child: Padding(
+                                        padding: const EdgeInsets.symmetric(
+                                            horizontal: 10, vertical: 15),
+                                        child: GridView.builder(
+                                          physics: BouncingScrollPhysics(),
+                                          gridDelegate:
+                                              SliverGridDelegateWithFixedCrossAxisCount(
+                                                  crossAxisCount: 2,
+                                                  childAspectRatio: 4.5,
+                                                  crossAxisSpacing: 10,
+                                                  mainAxisSpacing: 20),
+                                          itemCount: timeRange.length,
+                                          itemBuilder: (context, index) {
+                                            String range = timeRange[index];
+                                            bool isReserved = false;
+                                            print(controller.currentAppointment);
+                                            print("controller.currentAppointment");
+                                            try {
+                                              var reserve = controller
+                                                  .currentAppointment
+                                                  .firstWhere((element) =>
+                                                      element.time == range &&
+                                                      element.date == date);
+                                              isReserved = true;
+                                            } catch (err) {}
+                                    
+                                            return Container(
+                                                child: TimeButton(
+                                              text: range,
                                               onPressed: () {
-                                                controller.changeSelectedType(
-                                                    consult.type);
+                                                controller.changedSelectedTime(range);
                                               },
-                                              child: Container(
-                                                margin: EdgeInsets.only(
-                                                    left: 10,
-                                                    right: 0,
-                                                    top: 10,
-                                                    bottom: 10),
-                                                child: Row(
-                                                  mainAxisAlignment:
-                                                      MainAxisAlignment.start,
-                                                  children: [
-                                                    Text(
-                                                      "${consult.type}",
-                                                      textAlign:
-                                                          TextAlign.start,
-                                                    ),
-                                                    Padding(
-                                                      padding:
-                                                          const EdgeInsets.only(
-                                                              left: 90),
-                                                      child: Text(
-                                                        "R\$: 150,00",
-                                                        textAlign:
-                                                            TextAlign.start,
-                                                      ),
-                                                    ),
-                                                  ],
-                                                ),
-                                              )),
-                                        ],
+                                              reserved: isReserved,
+                                              selected:
+                                                  controller.selectedTime == range,
+                                            ));
+                                          },
+                                        ),
                                       ),
                                     ),
-                                  ),
-                                ),
-                              );
-                            }),
-                      )
-                    ],
+                                  ],
+                                );
+                              } catch (err) {
+                                return Text("Não possui consultas nesse dia");
+                              }
+                            }
+
+                            return Center(child: CircularProgressIndicator());
+                          },
+                        ),
+                        Container(
+                          height: controller.consultsType.length * 90,
+                          child: Expanded(
+                            child: ListView.builder(
+                                itemCount: controller.consultsType.length,
+                                itemBuilder: (context, index) {
+                                  ConsultModel consult =
+                                      controller.consultsType[index];
+                                  return Padding(
+                                    padding: const EdgeInsets.all(15),
+                                    child: PhysicalModel(
+                                      color: Colors.black,
+                                      elevation: 2,
+                                      borderRadius: BorderRadius.circular(10),
+                                      child: Container(
+                                        decoration: BoxDecoration(
+                                            color: Colors.white,
+                                            borderRadius:
+                                                BorderRadius.circular(10)),
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(10),
+                                          child: Column(
+                                            children: [
+                                              ElevatedButton(
+                                                  style: ButtonStyle(
+                                                      backgroundColor:
+                                                          MaterialStateProperty.all<
+                                                                  Color>(
+                                                              AppColors.darkBlue),
+                                                      shape: MaterialStateProperty
+                                                          .all<RoundedRectangleBorder>(
+                                                              RoundedRectangleBorder(
+                                                        borderRadius:
+                                                            BorderRadius.circular(
+                                                                10.0),
+                                                      ))),
+                                                  onPressed: () {
+                                                    controller.changeSelectedType(
+                                                        consult.type);
+                                                  },
+                                                  child: Container(
+                                                    margin: EdgeInsets.only(
+                                                        left: 10,
+                                                        right: 0,
+                                                        top: 10,
+                                                        bottom: 10),
+                                                    child: Row(
+                                                      mainAxisAlignment:
+                                                          MainAxisAlignment.start,
+                                                      children: [
+                                                        Text(
+                                                          "${consult.type}",
+                                                          textAlign:
+                                                              TextAlign.start,
+                                                        ),
+                                                        Padding(
+                                                          padding:
+                                                              const EdgeInsets.only(
+                                                                  left: 90),
+                                                          child: Text(
+                                                            "R\$: 150,00",
+                                                            textAlign:
+                                                                TextAlign.start,
+                                                          ),
+                                                        ),
+                                                      ],
+                                                    ),
+                                                  )),
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  );
+                                }),
+                          ),
+                        )
+                      ],
+                    ),
                   );
                 });
               }
