473e2c0b3889d81d6555e801b88ed4d6a28df17b
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2021-08-08 09:09:09 -0300

    removed Liked, added makeReview function

--- mobile/lib/Screens/MakeReview/controller/make_review_page_controller.dart
+++ mobile/lib/Screens/MakeReview/controller/make_review_page_controller.dart
@@ -1,14 +1,14 @@
+import 'package:Spital/Screens/MakeReview/controller/make_review_repository.dart';
 import 'package:mobx/mobx.dart';
 part 'make_review_page_controller.g.dart';
 
 class MakeReviewPageController = _MakeReviewPageControllerBase with _$MakeReviewPageController;
 
 abstract class _MakeReviewPageControllerBase with Store {
-  @observable
-  double rating = 2.5;
+  MakeReviewRepository _repository = MakeReviewRepository();
 
   @observable
-  int liked = -1;
+  double rating = 2.5;
 
   @observable
   String text = "";
@@ -19,15 +19,13 @@ abstract class _MakeReviewPageControllerBase with Store {
   @action
   changeRating(double value) => rating = value;
 
-  @action 
-  changeLiked(int value){
-    print(value);
-    liked = value;
-  }
-
   @action
   changeText(String value) => text = value;
 
   @action
   changeSpitalGrade(int value) => spitalGrade = value;
+
+  Future<bool> makeReview(int medicID, int clientID) async {
+    return await _repository.makeReview(clientID, medicID, text, rating);
+  }
 }
