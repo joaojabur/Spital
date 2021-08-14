3d5f44883d8ea153568d27ae460d38e006a11ffa
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2021-08-14 18:12:30 -0300

    Added Filter Dialog, Better List Scroll

--- mobile/lib/Screens/Home/Widget/TabPages/TabPageSeach/Widget/PageSeachDoctors/Widgets/page_seach_doctors.dart
+++ mobile/lib/Screens/Home/Widget/TabPages/TabPageSeach/Widget/PageSeachDoctors/Widgets/page_seach_doctors.dart
@@ -1,5 +1,10 @@
+import 'package:Spital/Screens/Home/Widget/TabPages/TabPageSeach/Widget/PageSeachDoctors/Widgets/controller.dart';
 import 'package:Spital/Screens/Home/Widget/TabPages/TabPageSeach/Widget/PageSeachDoctors/Widgets/list/list_doctor_by_area.dart';
+import 'package:Spital/Screens/Shared/Widgets/FilterDialog/filter_dialog.dart';
+import 'package:Spital/core/core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:provider/provider.dart';
 
 import 'appbar/sliver_app_bar_seach_doctors.dart';
 
@@ -14,11 +19,14 @@ class PageSeachDoctors extends StatefulWidget {
 }
 
 class _PageSeachDoctorsState extends State<PageSeachDoctors> {
+  final PageSearchController controller = PageSearchController();
   @override
   Widget build(BuildContext context) {
     String texto = widget.texto;
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
+
+
     return Scaffold(
         body: NestedScrollView(
             physics: BouncingScrollPhysics(),
@@ -30,15 +38,63 @@ class _PageSeachDoctorsState extends State<PageSeachDoctors> {
                   image: "${widget.image}",
                   width: width,
                   height: height,
+                  onFilterClick: (){
+                    showDialog(
+                      context: context,
+                      builder: (_) => Observer(
+                        builder: (_) {
+                          return FilterDialog(
+                            currentPrice: controller.price,
+                            currentDistance: controller.distance,
+                            onFilter: (int distance, int price){
+                              controller.changeDistance(distance);
+                              controller.changePrice(price);
+                              Navigator.pop(_);
+                            }
+                          );
+                        }
+                      )
+                    );
+                  },
+                  onTextChange: controller.changeName,
                 ),
               ];
             },
-            body: Column(
-              children: [
-                Expanded(
-                  child: ListDoctors(area: texto, page: 0),
-                ),
-              ],
-            )));
+            body: Observer(
+              builder: (_) {
+                return Column(
+                  children: [
+                  ListDoctors(
+                      area: texto, 
+                      distance: controller.distance,
+                      name: controller.name,
+                      page: controller.pages,
+                      price: controller.price,
+                      pageSearchController: controller,
+                    ),
+                    if (controller.lastSize == (30 * controller.pages)) ...([
+                      Container(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () => controller.changePage(controller.pages + 1),
+                          child: Text("Carregar mais"),
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(AppColors.darkBlue),
+                            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              )
+                            )
+                          ),
+                        ),
+                      )
+                    ])
+                  ]
+                );
+              }
+            )
+          )
+        );
   }
 }
