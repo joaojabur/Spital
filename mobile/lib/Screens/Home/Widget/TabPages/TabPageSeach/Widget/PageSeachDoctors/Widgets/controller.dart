4210119e43f6dc6df7ee1b9845faa76cb29ae2aa
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2021-08-14 18:11:52 -0300

    Created PageSearchController

---
+++ mobile/lib/Screens/Home/Widget/TabPages/TabPageSeach/Widget/PageSeachDoctors/Widgets/controller.dart
@@ -0,0 +1,39 @@
+import 'package:mobx/mobx.dart';
+part 'controller.g.dart';
+
+class PageSearchController = _PageSearchControllerBase with _$PageSearchController;
+
+abstract class _PageSearchControllerBase with Store {
+  @observable
+  int lastSize = 0;
+
+  @observable
+  int pages = 1;
+
+  @observable
+  int distance = 9999;
+
+  @observable
+  int price = 500;
+
+  @observable
+  String? name;
+
+  @action
+  changeLastSize(int value) => lastSize = value;
+
+  @action
+  changeDistance(int value) => distance = value;
+
+  @action
+  changeName(String value) => name = value;
+
+  @action
+  changePage(int value){
+    pages = value;
+    print("Offset $value");
+  }
+
+  @action
+  changePrice(int value) => price = value;
+}
